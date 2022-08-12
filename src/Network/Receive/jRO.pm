801b8c51e2f3cf6618dc4ac9bb333b52c96742ed
Yuanxiang Ye <yuanxiang.ye@ea.com>
2022-08-12 10:03:31 +0900

    [jRO] Update storage add item packet and jRO tables

--- src/Network/Receive/jRO.pm
+++ src/Network/Receive/jRO.pm
@@ -23,6 +23,7 @@ sub new {
 		'0A4D' => ['account_server_info', 'v a4 a4 a4 a4 a26 C a*', [qw(len sessionID accountID sessionID2 lastLoginIP lastLoginTime accountSex serverInfo)]],
 		'009D' => ['item_exists', 'a4 V C v3 C2', [qw(ID nameID identified x y amount subx suby)]],
 		'0ADD' => ['item_appeared', 'a4 V v C v2 C2 v C v', [qw(ID nameID type identified x y subx suby amount show_effect effect_type )]],
+		'0A0A' => ['storage_item_added', 'a2 V V C4 a16 a25', [qw(ID amount nameID type identified broken upgrade cards options)]],
 		'0A37' => ['inventory_item_added', 'a2 v V C3 a16 V C2 a4 v a25 C v', [qw(ID amount nameID identified broken upgrade cards type_equip type fail expire unknown options favorite viewID)]], # 69 bytes long and still a work in progress.
 	);
 
