35c305a145f83b0bb81f529727ac75c9bf78b2fa
hexhexD <alvin.ye.y@gmail.com>
2024-02-21 21:16:18 -0800

    Update

--- xuexi.pl
+++ xuexi.pl
@@ -5,7 +5,6 @@ use lib "$RealBin";
 use lib "$RealBin/src";
 use lib "$RealBin/src/deps";
 use LWP::UserAgent;
-use JSON::Tiny qw(encode_json);
 
 use Utils;
 
@@ -14,17 +13,14 @@ use FFI::Platypus;
 use Data::Printer;
 use Devel::StackTrace;
 
+use Utils qw(makeCoordsFromTo);
+
 # my $i = 0;
 # print STDERR "Stack Trace:\n";
 # while ( (my @call_details = (caller($i++))) ){
 # print STDERR $call_details[1].":".$call_details[2]." in function ".$call_details[3]."\n";
 # }
 
-my %firstHash = (
-	"Newton"	=>	"Issac",
-	"Einstein"	=>	"Albert",
-	1 =>	"Charles",
-);
 #
 my $freq = 750;
 my $duration = 300;
@@ -32,7 +28,6 @@ my $duration = 300;
 my $ffi = FFI::Platypus->new( api => 2, lib=>[undef], lang => 'Win32' );
 my $address = $ffi->find_symbol("MessageBoxA");
 print(sprintf("%X\n", $address));
-#
 
 use Devel::StackTrace;
 my $trace = Devel::StackTrace->new;
@@ -53,15 +48,48 @@ my %account = (
 	]
 );
 
+print("Account: " . p(%account) . "\n");
 assert("") if DEBUG;
 
-my %content = ('username' => '[OpenKore-Bot]', 'content' => "hahaahha");
-my $json = encode_json(\%content);
-# my $json = encode_json({content => "Me and Friend"});
-LWP::UserAgent->new->post(
-	'https://discord.com/api/webhooks/1171953378646032500/gOiKFII_3igNJhzlE5uGDCWUmqfwrPU2LdjPXdOv38-M-G6ozipsBjHev7s_GyUew2LY',
-	'Content-Type' => 'application/json',
-	'User-Agent' => 'Mozilla/4.0',
-	'Content' => "$json",
+my $something;
+if (defined $something) {
+	print "something is defined\n";
+} else {
+	print "something is not defined\n";
+}
+
+my $coords = "\x3D\x82\xA3\xDC\x2A\x88";
+my %from = (
+	"x" => 256,
+	"y" => 100,
 );
+my %to = (
+	"x" => 0,
+	"y" => 0,
+);
+
+my $char = {
+	"pos" => \%from,
+	"pos_to" => \%to
+};
+sub noparamundef {
+	my ($caller, $isDead) = @_;
+	if (defined $isDead) {
+		print "isDead is defined\n";
+	} else {
+		print "isDead is not defined\n";
+	}
+}
+
+noparamundef("yolo", 1);
+
+print $char->{pos} . "\n";
+%{$char->{pos}} = %to;
+print np($char->{pos}) . "\n";
+print $char->{pos} . "\n";
+# print np($char) . "\n";
+
+# my $ref = \%from;
+
+
 
